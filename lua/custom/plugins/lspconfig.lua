bb415e5c1340c178167d72df48660603c2b06d9e
xiang9.li <xiang9.li@ly.com>
2022-07-08 01:21:33 +0800

    lsp选中自动退出quickfix

--- lua/custom/plugins/lspconfig.lua
+++ lua/custom/plugins/lspconfig.lua
@@ -13,4 +13,8 @@ M.setup_lsp = function(attach, capabilities)
    end
 end
 
+-- 自动退出quickfix
+local autocmd = vim.api.nvim_create_autocmd
+autocmd({ "BufLeave" }, { pattern = { "*" }, command = "if &buftype == 'quickfix'|q|endif" })
+
 return M
